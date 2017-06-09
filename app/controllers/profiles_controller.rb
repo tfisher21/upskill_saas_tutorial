b75d075fa35487a85c8e600cac8e20893a3b8528
Tyler Fisher <tyler.r.fisher@icloud.com>
2017-06-09 00:55:45 +0000

    Added user profiles form.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
 	# GET request to '/users/:user_id/profile/new'.
 	def new
 		# Render a blank profile details form.
+		@profile = Profile.new
 	end
 end
